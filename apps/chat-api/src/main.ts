2b5d906a4f1eb19ef1ae70de83ff2f2c7e6ec8bb
Chau Tran <ctran@Chaus-MBP.lan>
2021-09-03 14:17:24 -0500

    feat: add bull dashboard

--- apps/chat-api/src/main.ts
+++ apps/chat-api/src/main.ts
@@ -1,6 +1,11 @@
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { ExpressAdapter } from '@bull-board/express';
 import { HttpStatus, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppConfig, appConfiguration } from '@nx-mess/chat-api/config/utils';
+import { USER_QUEUE } from '@nx-mess/chat-api/shared/utils';
+import { Queue } from 'bull';
 
 import * as compression from 'compression';
 import * as helmet from 'helmet';
@@ -8,8 +13,14 @@ import * as helmet from 'helmet';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+  const bullServerAdapter = new ExpressAdapter();
+
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get<AppConfig>(appConfiguration.KEY);
+
+  const queues = [USER_QUEUE].map(
+    (queueName) => new BullAdapter(app.get<Queue>(`BullQueue_${queueName}`))
+  );
 
   app.enableCors();
 
@@ -25,6 +36,14 @@ async function bootstrap() {
   app.use('/favicon.ico', (_, res) => {
     res.sendStatus(HttpStatus.NO_CONTENT).end();
   });
+
+  // BullBoard
+  createBullBoard({
+    queues,
+    serverAdapter: bullServerAdapter,
+  });
+  bullServerAdapter.setBasePath(`/admin/queues`);
+  app.use(`/admin/queues`, bullServerAdapter.getRouter());
 
   await app.listen(appConfig.port, () => {
     Logger.log(`Listening on: ${appConfig.domain}/${globalPrefix}`);
